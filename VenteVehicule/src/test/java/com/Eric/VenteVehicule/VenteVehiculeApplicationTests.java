aa3aba892af7f7de4fda0e793fbde4bcb1549529
EricMahafaly <Ericmahafaly7@gmail.com>
2024-01-06 19:03:35 +0100

    initialize

---
+++ VenteVehicule/src/test/java/com/Eric/VenteVehicule/VenteVehiculeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Eric.VenteVehicule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenteVehiculeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
