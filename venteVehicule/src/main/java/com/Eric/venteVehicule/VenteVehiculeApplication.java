43316204a04f84c10caad10f20b3c05c969949c2
EricMahafaly <Ericmahafaly7@gmail.com>
2024-01-07 20:31:22 +0100

    initialisation

---
+++ venteVehicule/src/main/java/com/Eric/venteVehicule/VenteVehiculeApplication.java
@@ -0,0 +1,14 @@
+package com.Eric.venteVehicule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class VenteVehiculeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenteVehiculeApplication.class, args);
+	}
+
+}
