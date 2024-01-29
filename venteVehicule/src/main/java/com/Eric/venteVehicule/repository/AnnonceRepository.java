1b963459d0fdab0d61e99d803d63b9e3361a70c1
EricMahafaly <Ericmahafaly7@gmail.com>
2024-01-29 07:52:06 +0100

    suppr

--- venteVehicule/src/main/java/com/Eric/venteVehicule/repository/AnnonceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Eric.venteVehicule.repository;
-
-import com.Eric.venteVehicule.model.Annonce;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnnonceRepository extends JpaRepository<Annonce, Integer> {
-}
