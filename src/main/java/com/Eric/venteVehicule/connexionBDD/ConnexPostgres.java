660045ff9a47392bf89c0e74ae03a57f7c5482c3
EricMahafaly <Ericmahafaly7@gmail.com>
2024-01-29 10:36:06 +0100

    Update ConnexPostgres.java

--- src/main/java/com/Eric/venteVehicule/connexionBDD/ConnexPostgres.java
+++ src/main/java/com/Eric/venteVehicule/connexionBDD/ConnexPostgres.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class ConnexPostgres {
     public Connection getConnex() {
-        String url = "jdbc:postgresql://localhost:5432/vente_vehicule";
+        String url = "jdbc:postgresql://roundhouse.proxy.rlwy.net:47730/railway";
         String username = "postgres";
-        String password = "root";
+        String password = "3AgFaC-fFCDGgag-6-FFd*FC4D1Ceg*g";
 
         Connection connection = null;
         try {
